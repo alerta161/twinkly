c520249405cbc9440e7848f64338d8437bf99e9e
alerta_161 <sashaalerta161>
2022-06-18 18:19:14 +0200

    ignor

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # twinkly
 #a website for a company made on jango with a feedback form 
-![](../../Users/sasha/OneDrive/Obrazy/копия.jpg)
-![](../../Users/sasha/OneDrive/Obrazy/коптя.jpg)
--- twinkly/twinkly/settings.py
+++ twinkly/twinkly/settings.py
@@ -33,7 +33,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "your@gmail.com"
+EMAIL_HOST_PASSWORD = "password"
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 # Application definition
 
 INSTALLED_APPS = [
